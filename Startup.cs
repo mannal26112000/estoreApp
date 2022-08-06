72589cc07e6096bb588cde69efcc2a3b0b7a7db2
mannal26112000 <105719185+mannal26112000@users.noreply.github.com>
2022-08-06 16:22:07 +0500

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Estorenew.Startup))]
+namespace Estorenew
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
